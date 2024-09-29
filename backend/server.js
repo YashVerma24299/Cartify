463240072e4c7d4173cebd3e586836763d593c43
YashVerma24299 <ritikverma23@gmail.com>
2024-09-29 23:31:08 +0530

    I have completed most of the backend development for a full-stack e-commerce website, utilizing Express.js, Stripe, MongoDB, and Redis.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticRoutes from "./routes/payment.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -23,10 +24,9 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);  
 app.use("/api/coupons", couponRoutes);  
 app.use("/api/payments", paymentRoutes);
-
+app.use("/api/analytics", analyticRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Server is running on port http://localhost:" + PORT);
-
     connectDB();
 });
