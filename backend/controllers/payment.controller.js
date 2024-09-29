0f7a003f77c275ef1c851e0442dd458001ccfa58
YashVerma24299 <ritikverma23@gmail.com>
2024-09-29 20:45:38 +0530

    add new payment system

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -25,7 +25,7 @@ export const createCheckoutSession = async (req, res) => {
 					},
 					unit_amount: amount,
 				},
-				quantity: product.quantity || 1,
+				//quantity: product.quantity || 1,
 			};
 		});
 
@@ -73,71 +73,71 @@ export const createCheckoutSession = async (req, res) => {
 	}
 };
 
-// export const checkoutSuccess = async (req, res) => {
-// 	try {
-// 		const { sessionId } = req.body;
-// 		const session = await stripe.checkout.sessions.retrieve(sessionId);
+export const checkoutSuccess = async (req, res) => {
+	try {
+		const { sessionId } = req.body;
+		const session = await stripe.checkout.sessions.retrieve(sessionId);
 
-// 		if (session.payment_status === "paid") {
-// 			if (session.metadata.couponCode) {
-// 				await Coupon.findOneAndUpdate(
-// 					{
-// 						code: session.metadata.couponCode,
-// 						userId: session.metadata.userId,
-// 					},
-// 					{
-// 						isActive: false,
-// 					}
-// 				);
-// 			}
+		if (session.payment_status === "paid") {
+			if (session.metadata.couponCode) {
+				await Coupon.findOneAndUpdate(
+					{
+						code: session.metadata.couponCode,
+						userId: session.metadata.userId,
+					},
+					{
+						isActive: false,
+					}
+				);
+			}
 
-// 			// create a new Order
-// 			const products = JSON.parse(session.metadata.products);
-// 			const newOrder = new Order({
-// 				user: session.metadata.userId,
-// 				products: products.map((product) => ({
-// 					product: product.id,
-// 					quantity: product.quantity,
-// 					price: product.price,
-// 				})),
-// 				totalAmount: session.amount_total / 100, // convert from cents to dollars,
-// 				stripeSessionId: sessionId,
-// 			});
+			// create a new Order
+			const products = JSON.parse(session.metadata.products);
+			const newOrder = new Order({
+				user: session.metadata.userId,
+				products: products.map((product) => ({
+					product: product.id,
+					quantity: product.quantity,
+					price: product.price,
+				})),
+				totalAmount: session.amount_total / 100, // convert from cents to dollars,
+				stripeSessionId: sessionId,
+			});
 
-// 			await newOrder.save();
+			await newOrder.save();
 
-// 			res.status(200).json({
-// 				success: true,
-// 				message: "Payment successful, order created, and coupon deactivated if used.",
-// 				orderId: newOrder._id,
-// 			});
-// 		}
-// 	} catch (error) {
-// 		console.error("Error processing successful checkout:", error);
-// 		res.status(500).json({ message: "Error processing successful checkout", error: error.message });
-// 	}
-// };
+			res.status(200).json({
+				success: true,
+				message: "Payment successful, order created, and coupon deactivated if used.",
+				orderId: newOrder._id,
+			});
+		}
+	} catch (error) {
+		console.error("Error processing successful checkout:", error);
+		res.status(500).json({ message: "Error processing successful checkout", error: error.message });
+	}
+};
 
-// async function createStripeCoupon(discountPercentage) {
-// 	const coupon = await stripe.coupons.create({
-// 		percent_off: discountPercentage,
-// 		duration: "once",
-// 	});
+async function createStripeCoupon(discountPercentage) {
+	const coupon = await stripe.coupons.create({
+		percent_off: discountPercentage,
+		duration: "once",
+	});
 
-// 	return coupon.id;
-// }
+	return coupon.id;
+};
 
-// async function createNewCoupon(userId) {
-// 	await Coupon.findOneAndDelete({ userId });
+async function createNewCoupon(userId) {
+	// await Coupon.findOneAndDelete({ userId });
 
-// 	const newCoupon = new Coupon({
-// 		code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
-// 		discountPercentage: 10,
-// 		expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
-// 		userId: userId,
-// 	});
+	const newCoupon = new Coupon({
+		code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+		discountPercentage: 10,
+		expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+		userId: userId,
+	});
 
-// 	await newCoupon.save();
+	await newCoupon.save();
 
-// 	return newCoupon;
-// }
+	return newCoupon;
+};
