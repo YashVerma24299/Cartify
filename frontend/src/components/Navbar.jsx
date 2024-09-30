ca16e333d1b82ec1eade4e52a23623f2ece2af9a
YashVerma24299 <ritikverma23@gmail.com>
2024-09-30 11:14:32 +0530

    Finally, i have started work on the frontend part and install the necessary library

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => { 
+    return (
+
+        <header >
+            <div className="">
+                
+            </div>
+        </header>
+    )
+}
+export default Navbar
