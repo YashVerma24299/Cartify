c07829d717832e4b33500718783dad30e34247c6
YashVerma24299 <ritikverma23@gmail.com>
2024-10-01 12:03:58 +0530

    Builded the Homepage and move to building the admin dashboard

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -37,4 +37,39 @@ export const useUserStore = create((set, get) => ({
 		}
 	},
 
+	logout: async () => {
+		try {
+			await axios.post("/auth/logout");
+			set({ user: null });
+		} catch (error) {
+			toast.error(error.response?.data?.message || "An error occurred during logout");
+		}
+	},
+
+	checkAuth: async () => {
+		set({ checkingAuth: true });
+		try {
+			const response = await axios.get("/auth/profile");
+			set({ user: response.data, checkingAuth: false });
+		} catch (error) {
+			console.log(error.message);
+			set({ checkingAuth: false, user: null });
+		}
+	},
+
+	// refreshToken: async () => {
+	// 	// Prevent multiple simultaneous refresh attempts
+	// 	if (get().checkingAuth) return;
+
+	// 	set({ checkingAuth: true });
+	// 	try {
+	// 		const response = await axios.post("/auth/refresh-token");
+	// 		set({ checkingAuth: false });
+	// 		return response.data;
+	// 	} catch (error) {
+	// 		set({ user: null, checkingAuth: false });
+	// 		throw error;
+	// 	}
+	// },
+
 }));
